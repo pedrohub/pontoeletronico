ecd0f7d32e2f09a13d7f70b7ee993f932707b9da
pedrohub <pedroedson.lima@gmail.com>
2017-11-26 22:46:35 -0200

    commit inicial do projeto

---
+++ src/main/java/com/springboot/pontoeletronico/api/PontoEletronicoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.pontoeletronico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletronicoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoEletronicoSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springboot/pontoeletronico/api/PontoEletronicoSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.pontoeletronico.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PontoEletronicoSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
