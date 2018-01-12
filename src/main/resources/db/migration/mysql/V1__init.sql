42c31adcbd60f0ede0636f5c8a054e462bce00a7
pedro edson <pedroedson.lima@gmail.com>
2018-01-12 00:07:34 -0200

    adicionado entidades e flyway

---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,64 @@
+-- Copiando estrutura do banco de dados para ponto_eletronico
+--CREATE DATABASE IF NOT EXISTS `ponto_eletronico` /*!40100 DEFAULT CHARACTER SET latin1 */;
+-- USE `ponto_eletronico`;
+
+-- Copiando estrutura para tabela ponto_eletronico.empresa
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cnpj` varchar(255) NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `razao_social` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela ponto_eletronico.empresa: ~0 rows (aproximadamente)
+DELETE FROM `empresa`;
+/*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela ponto_eletronico.funcionario
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cpf` varchar(255) NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) NOT NULL,
+  `perfil` varchar(255) NOT NULL,
+  `hora_almoco` float NOT NULL,
+  `hora_dia` float DEFAULT NULL,
+  `senha` varchar(255) NOT NULL,
+  `valor_hora` decimal(19,2) DEFAULT NULL,
+  `empresa_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK4cm1kg523jlopyexjbmi6y54j` (`empresa_id`),
+  CONSTRAINT `FK4cm1kg523jlopyexjbmi6y54j` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela ponto_eletronico.funcionario: ~0 rows (aproximadamente)
+DELETE FROM `funcionario`;
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela ponto_eletronico.lancamento
+CREATE TABLE IF NOT EXISTS `lancamento` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `data` datetime NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `localizacao` varchar(255) NOT NULL,
+  `tipo` varchar(255) NOT NULL,
+  `funcionario_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK46i4k5vl8wah7feutye9kbpi4` (`funcionario_id`),
+  CONSTRAINT `FK46i4k5vl8wah7feutye9kbpi4` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela ponto_eletronico.lancamento: ~0 rows (aproximadamente)
+DELETE FROM `lancamento`;
+/*!40000 ALTER TABLE `lancamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lancamento` ENABLE KEYS */;
+
+
